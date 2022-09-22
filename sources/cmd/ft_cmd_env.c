63d8f084b1f1abca09f533cc3f4d70efc357c254
exceed96 <90549959+exceed96@users.noreply.github.com>
2022-09-22 20:15:05 +0900

    Add files via upload

---
+++ sources/cmd/ft_cmd_env.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cmd_env.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangyeki <sangyeki@student.42seoul.kr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 11:41:54 by sangyeki          #+#    #+#             */
+/*   Updated: 2022/09/14 12:10:06 by sangyeki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+int	ft_cmd_env(t_data *data)
+{
+	char	**str[2];
+	int		i;
+
+	i = 0;
+	str[0] = ((t_cmd *)data->cmds->content)->f_cmd;
+	if (str[0][1])
+	{
+		printf("env : %s: No such file or directory\n", str[0][1]);
+		return (127);
+	}
+	while (data->env[i])
+	{
+		if (str[0][0][0] == 'e' && str[0][0][1] == 'x')
+			printf("declare -x ");
+		printf("%s\n", data->env[i++]);
+	}
+	return (0);
+}
